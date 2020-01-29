05437b96c0eb1ea8c4eeb3dabd067f6f5e121bd5
FlashB1T <60339756+FlashB1T@users.noreply.github.com>
2020-01-29 14:55:32 +0600

    pre-alpha v 0.00000000000000000000000000000000000001
    shs

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_direrctor = new System.Windows.Forms.Button();
+            this.button_coach = new System.Windows.Forms.Button();
+            this.button_manager = new System.Windows.Forms.Button();
+            this.button_user = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_direrctor
+            // 
+            this.button_direrctor.Location = new System.Drawing.Point(12, 12);
+            this.button_direrctor.Name = "button_direrctor";
+            this.button_direrctor.Size = new System.Drawing.Size(75, 23);
+            this.button_direrctor.TabIndex = 0;
+            this.button_direrctor.Text = "Дыректор ♂";
+            this.button_direrctor.UseVisualStyleBackColor = true;
+            this.button_direrctor.Click += new System.EventHandler(this.button_direrctor_Click);
+            // 
+            // button_coach
+            // 
+            this.button_coach.Location = new System.Drawing.Point(12, 41);
+            this.button_coach.Name = "button_coach";
+            this.button_coach.Size = new System.Drawing.Size(75, 23);
+            this.button_coach.TabIndex = 1;
+            this.button_coach.Text = "Тренер";
+            this.button_coach.UseVisualStyleBackColor = true;
+            this.button_coach.Click += new System.EventHandler(this.button_coach_Click);
+            // 
+            // button_manager
+            // 
+            this.button_manager.Location = new System.Drawing.Point(12, 70);
+            this.button_manager.Name = "button_manager";
+            this.button_manager.Size = new System.Drawing.Size(75, 23);
+            this.button_manager.TabIndex = 2;
+            this.button_manager.Text = "Менегер";
+            this.button_manager.UseVisualStyleBackColor = true;
+            this.button_manager.Click += new System.EventHandler(this.button_manager_Click);
+            // 
+            // button_user
+            // 
+            this.button_user.Location = new System.Drawing.Point(12, 99);
+            this.button_user.Name = "button_user";
+            this.button_user.Size = new System.Drawing.Size(75, 23);
+            this.button_user.TabIndex = 3;
+            this.button_user.Text = "Гачибасс♂";
+            this.button_user.UseVisualStyleBackColor = true;
+            this.button_user.Click += new System.EventHandler(this.button_user_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button_user);
+            this.Controls.Add(this.button_manager);
+            this.Controls.Add(this.button_coach);
+            this.Controls.Add(this.button_direrctor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_direrctor;
+        private System.Windows.Forms.Button button_coach;
+        private System.Windows.Forms.Button button_manager;
+        private System.Windows.Forms.Button button_user;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_direrctor_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_coach_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_manager_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_user_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
